bee755b8cd9ace605638f2f8ca04d87be9f51420
JuliaRokhlina <92213643+JuliaRokhlina@users.noreply.github.com>
2022-05-04 12:10:33 +0500

    new laptop

---
+++ My project/Assets/Scripts/Catalogues/LaptopCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+    public class LaptopCategory : MonoBehaviour
+    {
+        public ContainerTypes type;
+
+        public void OnMouseDown()
+        {
+            var ui = GameObject.Find("Laptop UI").GetComponent<LaptopUI>();
+            ui.category = type;
+            ui.PageUpdate(0);
+        }
+    }
+}
--- My project/Assets/Scripts/Catalogues/LaptopUI.cs
+++ My project/Assets/Scripts/Catalogues/LaptopUI.cs
@@ -12,20 +12,26 @@ namespace Game
 {
     public class LaptopUI : MonoBehaviour
     {
+        public ContainerTypes category;
         private void Start()
         {
             PageUpdate(0);
+            category = ContainerTypes.Coffin;
         }
-
-        private void PageUpdate(int page)
+        
+        public void PageUpdate(int page)
         {
-            var containers = GameState.Containers;
-            var y = 4;
             var canvas = GameObject.Find("Canvas");
+            foreach (Transform child in canvas.transform) {
+                Destroy(child.gameObject);
+            }
+            var containers = GameState.Containers;
+            var x = -4.7f;
+            var y = 3.5f;
             for (var i = page * 3; i < Math.Min(page * 3 + 3, containers.Count); i++)
             {
                 var container = containers[i];
-                if (container.Type != ContainerTypes.Coffin)
+                if (container.Type != category)
                     continue;
                 
                 var sprite = Resources.Load<Sprite>(container.Image);
@@ -33,20 +39,20 @@ namespace Game
                 {
                     var image = (GameObject)Instantiate(Resources.Load("ShopImage"), canvas.transform);
                     image.GetComponent<Image>().sprite = sprite;
-                    image.transform.position = new Vector3(-5.8f, y, 0);
+                    image.transform.position = new Vector3(x, y, 0);
                 }
 
                 var contName = (GameObject)Instantiate(Resources.Load("ShopText"), canvas.transform);
                 contName.GetComponent<Text>().text = container.Name;
-                contName.transform.position = new Vector3(0, y, 0);
+                contName.transform.position = new Vector3(x + 5.8f, y, 0);
                 
                 var price = (GameObject)Instantiate(Resources.Load("ShopText"), canvas.transform);
                 price.GetComponent<Text>().text = container.Price + "$";
-                price.transform.position = new Vector3(3, y, 0);
+                price.transform.position = new Vector3(x + 8.3f, y, 0);
                 
                 var button = (GameObject)Instantiate(Resources.Load("ShopButton"), canvas.transform);
                 button.GetComponent<ShopButton>().Preference = container;
-                button.transform.position = new Vector3(5, y, 0);
+                button.transform.position = new Vector3(x + 10.3f, y, 0);
 
                 y -= 3;
             }
